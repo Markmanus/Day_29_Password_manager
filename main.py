728e0d7d06e7fd320d86df3779c0e7d06172f85e
Markmanus <pampadalampa@gmail.com>
2023-07-26 00:27:46 +0100

    Added paperclyp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 import pyperclip
 import json
 
@@ -8,6 +8,26 @@ import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+
+def generate_password():
+    password_letters = [choice(letters) for letter in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for symbol in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for number in range(randint(2, 4))]
+
+    password_list = password_numbers + password_symbols + password_letters
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -15,11 +35,23 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
-        messagebox.showinfo(title="Success", message="Password saved")
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                            f"\nPassword: {password} \nIs it ok to save?")
+    if is_ok:
+        if len(website) == 0 or len(password) == 0:
+            messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty")
+        elif "@" not in email:
+            messagebox.showinfo(title="Oops", message="Please make sure you have entered a valid email")
+        else:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                messagebox.showinfo(title="Success", message="Password saved")
+    else:
+        pass
+
+
 
     ############################## JSON better option, tested ##############################
     # new_data = {
@@ -87,9 +119,10 @@ email_entry.insert(0, "insert email")
 
 
 password_entry = Entry(width=21 ,bg="white", borderwidth=1, relief="solid")
+
 password_entry.grid(column=1, row=3, sticky="EW")
 #buttons
-generate_password_button = Button(width=14 ,text="Generate Password", bg="white")
+generate_password_button = Button(width=14 ,text="Generate Password", bg="white", command=generate_password)
 generate_password_button.grid(column=2, row=3, sticky="W")
 add_button = Button(width=36, text="Add", bg="white", command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="W")
